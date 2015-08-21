a909e98d21c11c6d1ec89ebf512b37f9602a02db
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-08-21 06:39:53 -0400

    added player clickable planet

--- js/main.js
+++ js/main.js
@@ -3,15 +3,27 @@ var mainApp = angular.module('mainApp', []);
 mainApp.controller('MainAppController', function() {
 	this.resources = 0;
 	this.turn = 1;
+	this.workQueue = 0;
 
 	this.endTurn = function() {
 		this.resources += 7
 		this.turn++;
+		endTurnModal();
+	};
+
+	this.buildShip = function() {
+		this.workQueue++;
+	};
+
+	var endTurnModal = function() {
+		if ($("#endTurnModal").length == 0)
+			return;
 		$("#endTurnModal").modal('show');
 		setTimeout(function(){
 			$("#endTurnModal").modal('hide');
 		}, 100);
-	};
+	}
+
 
 	var victory = function() {
 		$("#victoryModal").modal('show');
--- test/main.Spec.js
+++ test/main.Spec.js
@@ -1,24 +1,30 @@
 describe('MainAppController', function() {
 	beforeEach(module('mainApp'));
 
-	var $controller;
+	var $controller, controller;
 
 	beforeEach(inject(function(_$controller_) {
 		// The injector unwraps the underscores (_) from around the parameter names when matching
 		$controller = _$controller_;
+		controller = $controller('MainAppController', { });
 	}));
 
 	describe('$controller.endTurn', function() {
 		it('ending turn adds resources', function() {
-			var controller = $controller('MainAppController', { });
 			controller.endTurn();
 			expect(controller.resources).toEqual(7);
 		});
 
 		it('ending turn increments turn counter', function() {
-			var controller = $controller('MainAppController', { });
 			controller.endTurn();
 			expect(controller.turn).toEqual(2);
+		});
+	});
+
+	describe('$controller.buildShip', function() {
+		it('requesting ship queues one ship to be built', function() {
+			controller.buildShip();
+			expect(controller.workQueue).toEqual(1);
 		});
 	});
 });
