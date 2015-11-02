51ae46e85d30e8cab5a480eb022e2dc860270e41
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-11-01 19:52:32 -0500

    Revert "temp fix for travis"
    This reverts commit 664cdc6786bcbbcf85a666ea46a9c8a91d3dbfce.

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,10 @@ module.exports = function (config) {
       'test/*.Spec.js'
     ],
     exclude: [],
-    reporters: ['progress'],
+    preprocessors: {
+      'js/*.js': ['coverage']
+    },
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
